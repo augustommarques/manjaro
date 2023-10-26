779bfbbc6ed418acc28478b60d32663f3efbddd2
Augusto Marques <augusto.mendes@ymail.com>
2023-10-25 21:44:40 -0300

    Atualizado

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Atualizar o sistema
 sudo pacman -Syu
 
-# Importar a chave GPG necessária
-gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
 
-# Criar o diretório de instalação
 mkdir -p ~/install
 cd ~/install
 
-# Lista de URLs
+gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+
 urls=(
-    "firefox-34-bin"
+    "firefox-bin"
     "tor-browser-bin"
     "dbeaver-git"
     "evolution-git"
@@ -25,22 +22,21 @@ urls=(
     "wine-git"
 )
 
-# Loop para clonar e instalar os pacotes
 for package_name in "${urls[@]}"; do
-    # Verificar se o pacote já está instalado
     if pacman -Qs "^$package_name$" > /dev/null; then
         echo "$package_name já está instalado."
     else
-        # Clonar o repositório
+
+        dependencies=$(yay -Si "$package_name" --printformat "%D" | grep -oP '^\S+')
+        
+        sudo pacman -S --noconfirm $dependencies
+
         git clone "https://aur.archlinux.org/$package_name.git"
 
-        # Entrar na pasta do repositório
         cd "$package_name"
 
-        # Construir e instalar o pacote sem pedir confirmação
         makepkg -si
 
-        # Voltar para o diretório de instalação
         cd ..
         sudo rm -R "$package_name"
     fi
